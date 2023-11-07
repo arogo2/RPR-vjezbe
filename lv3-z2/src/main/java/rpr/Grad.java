d4eeafd9161d7ee3d44da3d7ed6c288282994851
arogo2 <arogo2@etf.unsa.ba>
2023-11-07 16:23:07 +0100

    Zadatak3

--- lv3-z2/src/main/java/rpr/Grad.java
+++
@@ -1,2 +0,0 @@
-package rpr;
-public enum Grad {SARAJEVO, TUZLA, ZENICA, BRCKO}
--- lv3-z2/src/main/java/rpr/TelefonskiBroj.java
+++
@@ -1,6 +0,0 @@
-package rpr;
-import java.util.*;
-public abstract class TelefonskiBroj {
-    public abstract String ispisi();
-    public abstract int hashCode();
-}
