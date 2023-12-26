98388415f3bab6ff0d5d2999197483a45b3caafa
arogo2 <148486866+arogo2@users.noreply.github.com>
2023-12-26 10:45:36 +0100

    Delete demo directory

--- demo/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    opens ba.unsa.etf.rpr to javafx.fxml;
-    exports ba.unsa.etf.rpr;
-}
